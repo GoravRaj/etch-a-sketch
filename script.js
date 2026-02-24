f6fd6808e442334cd93332701ef3f39ed1457cab
GoravRAj <goravtriton@gmail.com>
2026-02-24 07:59:33 +0530

    Completed styling for the project
    Also added css to prevent text/element selection with cursor drag

---
+++ script.js
@@ -0,0 +1,5 @@
+const grid = document.getElementById("grid-container");
+const slider = document.getElementById("size-slider");
+const sizeText = document.getElementById("size-text");
+const colorButtons = document.querySelectorAll(".color-btn");
+
